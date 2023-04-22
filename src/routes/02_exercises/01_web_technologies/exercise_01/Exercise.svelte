de28a6f7024f21e8cb6c4dfa88251eb8db5ad871
dindaprayunita <dindaprayunita@gmail.com>
2023-04-22 18:25:56 +0200

    finalized ex1

--- src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
@@ -1,14 +1,1 @@
 <div class="answer" style="color:green;">My name is <b style="color:red;">Dinda</b></div>
-<div class="answer" id="answer2" style="color:green;">bablabllblb <b style="color:red;">Dinda</b></div>
-
-<style>
-  .answer {
-    background-color: red;
-    padding: 5px;
-  }
-
-  #answer2 {
-    background-color: green;
-  }
-  
-</style>
