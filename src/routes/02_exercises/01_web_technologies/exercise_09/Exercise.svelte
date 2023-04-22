514f1243cd7cd3c43426b464262dacca01213aae
dindaprayunita <dindaprayunita@gmail.com>
2023-04-22 19:01:54 +0200

    ex9

---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,8 @@
+<script>
+    let radius = 10;
+    function calc_circ(radius) {
+        circ = radius * 2 * 3.14
+        return Math.round(circ, 2);
+    }
+</script>
+The circumference of a circle with radius {radius}m is {calc_circ(radius)}m
